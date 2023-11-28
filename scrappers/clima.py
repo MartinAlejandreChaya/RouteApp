217601731d96e1a3e3c6353d9d6fbfe5ba321102
laurascotta19 <laurasc2001@gmail.com>
2023-11-28 18:07:49 +0100

    holi no quiero estorba

--- scrappers/clima.py
+++ scrappers/clima.py
@@ -1,14 +1,14 @@
 
 # Script that gets the expected climate on location `loc` from aemet API
 # Returns: {"sucess": True/False, "climate": climate_data}
-def get_clima(loc):
+from aemet import Aemet,Municipio,Prediccion
+aemet_client=Aemet(api_key='eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsYXVyYXNjMjAwMUBnbWFpbC5jb20iLCJqdGkiOiIxNjIyZmI4ZS05YWQ1LTQwOGMtODc1Zi1iNDU4OWY3NmQyMWMiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTcwMDU4OTM5NSwidXNlcklkIjoiMTYyMmZiOGUtOWFkNS00MDhjLTg3NWYtYjQ1ODlmNzZkMjFjIiwicm9sZSI6IiJ9.-Ega71ci8LU5Ss2ZiYyRsyQQUEoHzisddpn9iH2BpU8')
 
-    if (not loc):
-        return {
-            "success": False,
-            "error_msg": "Ubicación de inicio de la ruta nó valida"
-        }
+def get_clima():
+    muns=Municipio.buscar('Manresa')
+    print(Municipio.get_municipio('08113'))
     
-    # Programa mar y laura
+    # Programa mar y laura22
 
-    return {"success": False, "error_msg": "Clima function not implemented yet"}
+    #return {"success": False, "error_msg": "Clima function not implemented yet"}
+get_clima()
