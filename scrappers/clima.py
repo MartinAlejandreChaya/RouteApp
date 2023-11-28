4f26279e74e1596e7085e0b0783112ecd26c09ae
laurascotta19 <laurasc2001@gmail.com>
2023-11-28 18:45:16 +0100

    Laura commit

--- scrappers/clima.py
+++ scrappers/clima.py
@@ -1,10 +1,12 @@
 
 # Script that gets the expected climate on location `loc` from aemet API
 # Returns: {"sucess": True/False, "climate": climate_data}
-import json
+
 from aemet import Aemet,Municipio, Prediccion
 aemet_client=Aemet(api_key='eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsYXVyYXNjMjAwMUBnbWFpbC5jb20iLCJqdGkiOiIxNjIyZmI4ZS05YWQ1LTQwOGMtODc1Zi1iNDU4OWY3NmQyMWMiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTcwMDU4OTM5NSwidXNlcklkIjoiMTYyMmZiOGUtOWFkNS00MDhjLTg3NWYtYjQ1ODlmNzZkMjFjIiwicm9sZSI6IiJ9.-Ega71ci8LU5Ss2ZiYyRsyQQUEoHzisddpn9iH2BpU8')
 
+def get_clima():
+    mun=Municipio.buscar('Manresa')
+    print(Municipio.get_municipio('08113'))
 
-if _name_ =='_clima_':
-    ruta=''
+get_clima
