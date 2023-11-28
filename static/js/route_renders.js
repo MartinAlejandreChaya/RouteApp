924c269c58d0beb56808bd50ef6b32955f589768
Guillesit <100518581@alumnos.uc3m.es>
2023-11-28 17:55:14 +0100

    un poco de frontend

--- static/js/route_renders.js
+++ static/js/route_renders.js
@@ -85,10 +85,69 @@ function createRouteItem(route) {
     // REST
     const rest_div = document.createElement("div");
 
-    const p = document.createElement("p");
-    p.innerHTML = "Aquí iría el resto de información relevante sobre la ruta (mapas, punto de partida, hoteles...)"
+    const div_importante = document.createElement("div");
+    const pp1= document.createElement("p");
+    pp1.innerHTML = "Dificultad: <b>" + route["route_data"]["dificultad_tecnica"] + "</b>";
+    const pp2 = document.createElement("p");
+    pp2.innerHTML = "Distancia: <b>" + route["route_data"]["distancia"] + "</b>";
+    const pp3 = document.createElement("p");
+    pp3.innerHTML = "Tipo de ruta: <b>" + route["route_data"]["tipo_ruta"] + "</b>";
+    const pp8 = document.createElement("p");
+    pp8.innerHTML = "Trailrank: <b>" + route["route_data"]["trailrank"] + "</b>";
+    
 
-    rest_div.appendChild(p);
+    const div_1 = document.createElement("div");
+    const pp4 = document.createElement("p");
+    pp4.innerHTML = "Altitud máxima: <b>" + route["route_data"]["altitud_maxima"] + "</b>";
+    const pp5 = document.createElement("p");
+    pp5.innerHTML = "Altitud mínima: <b>" + route["route_data"]["altitud_minima"] + "</b>";
+    
+    const div_2 = document.createElement("div");
+    const pp6 = document.createElement("p");
+    pp6.innerHTML = "Desnivel negativo: <b>" + route["route_data"]["desnivel_negativo"] + "</b>";
+    const pp7 = document.createElement("p");
+    pp7.innerHTML = "Desnivel positivo: <b>" + route["route_data"]["desnivel_positivo"] + "</b>";
+    
+    
+    const pp9 = document.createElement("p");
+    pp9.innerHTML = "Página de descarga: <a href=" + route["route_data"]["pagina_descarga"] + " target='_blank'>" + "Link de descarga" + "</a>";
+
+    
+    div_importante.appendChild(pp1);
+    div_importante.appendChild(pp2);
+    div_importante.appendChild(pp3);
+    div_importante.appendChild(pp8);
+    rest_div.appendChild(div_importante);
+
+    
+    div_1.appendChild(pp4);
+    div_1.appendChild(pp5);
+    rest_div.appendChild(div_1);
+
+    div_2.appendChild(pp6);
+    div_2.appendChild(pp7);
+    rest_div.appendChild(div_2);
+
+    
+    rest_div.appendChild(pp9);
+
+    if (route["route_data"]["estrellas"]){
+        const pp0 = document.createElement("p");
+        pp0.innerHTML = "Estrellas: <b>" + route["route_data"]["estrellas"] + "</b>";
+        rest_div.appendChild(pp0);
+    }
+    if (route["alojamientos"]){
+        const pp10 = document.createElement("p");
+        pp20.innerHTML = "Estrellas: <b>" + route["route_data"]["estrellas"] + "</b>";
+        rest_div.appendChild(pp10);
+    }
+    if (route["clima"]){
+        const pp20 = document.createElement("p");
+        pp20.innerHTML = "Estrellas: <b>" + route["route_data"]["estrellas"] + "</b>";
+        rest_div.appendChild(pp20);
+    }
+
+
 
 
     cont.appendChild(title_div);
